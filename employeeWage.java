947c42e53a3d8ac3e52ce6c8397030cc4df99b0e
akshayzagad <akshayzagade62@gmail.com.com>
2022-09-29 23:47:01 +0530

    UC 4 total Employee attendence and wage

--- employeeWage.java
+++ employeeWage.java
@@ -8,20 +8,32 @@ public class employeeWage {
     static final int Is_part_time=2;
     static final  int Wage_of_employee=0;
     static final int Wage_of_employee_Part_Timer=0;
+    static final int Working_Days_per_Month=20;
     public static void main(String[] args) {
-        int employeeAttendance= (int)Math.floor(Math.random()*10)%3;
-        switch (employeeAttendance){
-            case Is_full_Time:
-                int   Wage_of_employee=Wage_Per_Hour*Full_Day_Hour;
-                System.out.println("employee is present and full time");
-            System.out.println("Wage_of_employee is "+Wage_of_employee);
-            break;
-            case Is_part_time  :
-                int Wage_of_employee_Part_Timer=Part_Time_Wage_Per_Hour*Wage_Per_Hour;
-           System.out.println("employee is present and Part time");
-          System.out.println("Wage_of_employee is "+Wage_of_employee_Part_Timer);
-            default:
-                System.out.println("employee is absent");
+        int day =1;
+        int Total_Wage=0;
+        int Daily_wage=0;
+        while (day<=Working_Days_per_Month) {
+            int employeeAttendance= (int)Math.floor(Math.random()*10)%3;
+            switch (employeeAttendance) {
+                case Is_full_Time:
+                    Daily_wage = Wage_Per_Hour * Full_Day_Hour;
+                  System.out.println("employee is present and full time");
+//                    System.out.println("Wage_of_employee is " + Wage_of_employee);
+                    break;
+                case Is_part_time:
+                    Daily_wage  = Part_Time_Wage_Per_Hour * Wage_Per_Hour;
+                  System.out.println("employee is present and Part time");
+//                    System.out.println("Wage_of_employee is " + Wage_of_employee_Part_Timer);
+                    break;
+                default:
+                    System.out.println("employee is absent");
+            }
+            Total_Wage=Total_Wage+Daily_wage;
+//            Total_Wage=Total_Wage+Wage_of_employee_Part_Timer;
+            day++;
+            System.out.println("Total_Wage_of_Employee " + Daily_wage);
         }
+          System.out.println("Total_wage == "+Total_Wage);
     }
 }
